70ea6196f6750077d8fc4421b56c77b7e0756b37
Joe Versoza <joeversoza@gmail.com>
2018-04-05 15:33:32 -0400

    fix up link

--- assignments/hw07/in_the_style_of.py
+++ assignments/hw07/in_the_style_of.py
@@ -3,6 +3,6 @@ in_the_style_of.py
 =====
 Create a drawing/sketch in the style of some visual art that you like. For
 example, you can try making a sketch that looks like a Piet Mondrian
-painting, or you can experiment with Truchet tiles! This is pretty freeform,
+painting, or you can experiment with Truchet tiles. This is pretty free-form,
 so there are no other requirements!
 """
